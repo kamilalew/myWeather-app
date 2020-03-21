aabd48ae7537b13147db8f8d6c1e46803a1418ad
Kamila <kamila.m.lewicka@gmail.com>
2020-03-21 13:59:21 +0000

    additiona changes to hardoced data

--- src/MyWeather/MyWeatherResults.js
+++ src/MyWeather/MyWeatherResults.js
@@ -5,7 +5,11 @@ export default function MyWeatherResults() {
     return (
         <div>
             <h1 className='city'>New York</h1>
-            <h1 className='temperature'>25&deg;</h1>
+            <div className='temperatures'>
+                <sub className='minTemperature'>23&deg;</sub>
+                <h1 className='temperature'>25&deg;</h1>
+                <sub className='maxTemperature'>27&deg;</sub>
+            </div>
             <img className='images' alt='sunny' src={sunny} />
         </div>
     )
