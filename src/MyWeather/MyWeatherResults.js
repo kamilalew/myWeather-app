399218be5b989b7cb829e3b3ba654e66e5542137
Kamila <kamila.m.lewicka@gmail.com>
2020-04-02 17:49:57 +0100

    Weather Icons edited

--- src/MyWeather/MyWeatherResults.js
+++ src/MyWeather/MyWeatherResults.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import Clouds from '../icons/Clouds.png';
 import Clear from '../icons/Clear.png';
+import Thunderstorm from '../icons/Thunderstorm.png';
+import Drizzle from '../icons/Drizzle.png';
+import Rain from '../icons/Rain.png';
+import Snow from '../icons/Snow.png';
+import Mist from '../icons/Mist.png';
+import Smoke from '../icons/Smoke.png';
+import Haze from '../icons/Haze.png';
+import Dust from '../icons/Dust.png';
+import Fog from '../icons/Fog.png';
+import Sand from '../icons/Sand.png';
+import Ash from '../icons/Ash.png';
+import Squall from '../icons/Squall.png';
+import Tornado from '../icons/Tornado.png';
+
+
 
 const iconMapping = {
     Clouds: Clouds,
-    cloudy: Clouds,
-    clear: Clear
+    Clear: Clear,
+    Thunderstorm: Thunderstorm,
+    Drizzle: Drizzle,
+    Rain: Rain,
+    Snow: Snow,
+    Mist: Mist,
+    Smoke: Smoke,
+    Haze: Haze,
+    Dust: Dust,
+    Fog: Fog,
+    Sand: Sand,
+    Ash: Ash,
+    Squall: Squall,
+    Tornado: Tornado
 };
 
 export default function MyWeatherResults(props) {
